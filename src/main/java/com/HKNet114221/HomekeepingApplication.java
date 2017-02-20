1eed1fc55201c63192779f8d5ba2b446daec72c6
xgpeng <xiangang.peng@handpay.com.cn>
2017-02-20 10:44:45 +0800

    *start

---
+++ src/main/java/com/HKNet114221/HomekeepingApplication.java
@@ -0,0 +1,12 @@
+package com.HKNet114221;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomekeepingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomekeepingApplication.class, args);
+	}
+}
